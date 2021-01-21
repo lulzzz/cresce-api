acebcd86c731534625d145f762b8203548441155
SergioFerreira <sergio.ferreira@ctw.bmwgroup.com>
2021-01-21 14:50:29 +0000

    bump version

---
+++ Cresce.Api/bump-version.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+function getVersion {
+input="appsettings.json"
+while IFS= read -r line
+do
+  if [[ $line == *"Version"* ]]; then
+    echo "$line"
+    return
+  fi
+
+done < "$input"
+}
+
+function increment_version
+{
+  declare -a part=( ${1//\./ } )
+  declare    new
+  declare -i carry=1
+
+  for (( CNTR=${#part[@]}-1; CNTR>=0; CNTR-=1 )); do
+    len=${#part[CNTR]}
+    new=$((part[CNTR]+carry))
+    [ ${#new} -gt $len ] && carry=1 || carry=0
+    [ $CNTR -gt 0 ] && part[CNTR]=${new: -len} || part[CNTR]=${new}
+  done
+  new="${part[*]}"
+  echo -e "${new// /.}"
+}
+
+version=$(echo $(getVersion) | sed 's/"Version": "//g' | sed 's/",//g')
+
+
+newVersion=$(echo $(increment_version $version))
+
+echo "$version > $newVersion"
+
+sed "s/$version/$newVersion/" appsettings.json > changed.txt && mv changed.txt appsettings.json
