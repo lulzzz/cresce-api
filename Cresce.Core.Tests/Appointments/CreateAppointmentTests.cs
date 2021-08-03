7abfa180f1a33ef520011dd08edb442203dc8496
SergioFerreira <sergio.ferreira@ctw.bmwgroup.com>
2021-08-03 13:30:52 +0100

    added validations for NewAppointment

--- Cresce.Core.Tests/Appointments/CreateAppointmentTests.cs
+++ Cresce.Core.Tests/Appointments/CreateAppointmentTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Cresce.Core.Appointments;
 using Cresce.Core.Authentication;
@@ -15,6 +16,8 @@ namespace Cresce.Core.Tests.Appointments
             {
                 CustomerId = 1,
                 ServiceId = 1,
+                From = new DateTime(2021, 01, 01),
+                To = new DateTime(2021, 01, 01)
             };
 
             await services.CreateAppointment(appointment, GetEmployeeAuthorization());
@@ -26,7 +29,9 @@ namespace Cresce.Core.Tests.Appointments
                 CustomerId = 1,
                 ServiceId = 1,
                 Color = "0xFF2196F3",
-                EventName = "Diogo Quintas\nDevelopment"
+                EventName = "Diogo Quintas\nDevelopment",
+                From = new DateTime(2021, 01, 01),
+                To = new DateTime(2021, 01, 01)
             });
         }
 
@@ -38,6 +43,8 @@ namespace Cresce.Core.Tests.Appointments
             {
                 CustomerId = 1,
                 ServiceId = 1,
+                From = new DateTime(2021, 01, 01),
+                To = new DateTime(2021, 01, 01)
             };
 
             var createdAppointment = await services.CreateAppointment(appointment, GetEmployeeAuthorization());
@@ -49,8 +56,74 @@ namespace Cresce.Core.Tests.Appointments
                 CustomerId = 1,
                 ServiceId = 1,
                 Color = "0xFF2196F3",
-                EventName = "Diogo Quintas\nDevelopment"
+                EventName = "Diogo Quintas\nDevelopment",
+                From = new DateTime(2021, 01, 01),
+                To = new DateTime(2021, 01, 01)
             }));
+        }
+
+        [Test]
+        public void Creating_an_Appointment_without_service_id_throws_exception()
+        {
+            var services = MakeService();
+            var appointment = new Appointment
+            {
+                CustomerId = 1,
+                From = new DateTime(2021, 01, 01),
+                To = new DateTime(2021, 01, 01)
+            };
+
+            Assert.CatchAsync<InvalidEntityException>(() =>
+                services.CreateAppointment(appointment, GetEmployeeAuthorization())
+            );
+        }
+
+        [Test]
+        public void Creating_an_Appointment_without_customer_id_throws_exception()
+        {
+            var services = MakeService();
+            var appointment = new Appointment
+            {
+                ServiceId = 1,
+                From = new DateTime(2021, 01, 01),
+                To = new DateTime(2021, 01, 01)
+            };
+
+            Assert.CatchAsync<InvalidEntityException>(() =>
+                services.CreateAppointment(appointment, GetEmployeeAuthorization())
+            );
+        }
+
+        [Test]
+        public void Creating_an_Appointment_with_from_date_lower_than_2000_throws_exception()
+        {
+            var services = MakeService();
+            var appointment = new Appointment
+            {
+                CustomerId = 1,
+                ServiceId = 1,
+                To = new DateTime(2021, 01, 01)
+            };
+
+            Assert.CatchAsync<InvalidEntityException>(() =>
+                services.CreateAppointment(appointment, GetEmployeeAuthorization())
+            );
+        }
+
+        [Test]
+        public void Creating_an_Appointment_with_to_date_lower_than_2000_throws_exception()
+        {
+            var services = MakeService();
+            var appointment = new Appointment
+            {
+                CustomerId = 1,
+                ServiceId = 1,
+                From = new DateTime(2021, 01, 01),
+            };
+
+            Assert.CatchAsync<InvalidEntityException>(() =>
+                services.CreateAppointment(appointment, GetEmployeeAuthorization())
+            );
         }
 
         [Test]
